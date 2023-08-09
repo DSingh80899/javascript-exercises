59812077c21a22ccc248b016af7b94fc9a935a10
D Singh <leveragegroupinc@gmail.com>
2023-08-09 13:20:48 -0700

    Solve excercise 1 to 7 javascript

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    
+    if(typeof a != "number" || typeof b != "number" || a<0 || b<0)
+        return 'ERROR'
+    let sum=0
+    if(a<b){
+        for(let i=a;i<=b;i++){
+            sum+=i
+        }
+    }
+    else{
+        for(let i=b;i<=a;i++){
+            sum+=i
+        }
+    }
+        return sum
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+
+    return Math.round((tempFahr-32)/1.8*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+
+    return Math.round((tempCel*1.8+32)*10)/10
 };
 
 // Do not edit below this line
