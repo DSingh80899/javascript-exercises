c8ec1613c7c489ee97ae77e676faa4fbfdcdd87b
D Singh <leveragegroupinc@gmail.com>
2023-08-11 13:39:19 -0700

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const ordered = people.sort((a,b)=> 
+    {
+        if(a.yearOfDeath == undefined) a.yearOfDeath= new Date().getFullYear()                
+        if(b.yearOfDeath == undefined) b.yearOfDeath= new Date().getFullYear()
+
+        if((a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth))
+            return -1
+        else
+            return 1
+    })
+    
+    
+    return ordered[0]
 };
 
 // Do not edit below this line
