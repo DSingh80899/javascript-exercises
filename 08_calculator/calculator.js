501768e141820ce397214756cd5fc28b12543ae1
D Singh <leveragegroupinc@gmail.com>
2023-08-11 12:49:42 -0700

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b
+};
+
+const subtract = function(a,b) {
+
+  return a-b
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
 	
+  return array.reduce((total,number) => total+number,0)
+  
 };
 
-const sum = function() {
-	
-};
+const multiply = function(array) {
 
-const multiply = function() {
+  return array.reduce((total,number) => total*number,1)
 
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+  let result=1
+
+  for(let i=0;i<power;i++){
+    result= result*base
+  }
+	return result
 };
 
-const factorial = function() {
+const factorial = function(number) {
+
+  let result=1
+  for(let i=1;i<=number;i++){
+    result= result*i
+  }
+  return result
+
 	
 };
 
